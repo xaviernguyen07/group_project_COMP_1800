efc6638d03b9f3193913fb4700f081724c5edc57
Tommy-81321 <56455799+Tommy-81321@users.noreply.github.com>
2020-03-14 14:37:45 -0700

    put style together

--- script.js
+++ script.js
@@ -1,106 +1,112 @@
 $(document).ready(function() {
-   /*
-   date/time picker
-   */
-   $('#date').pickadate( {
-      formatSubmit: 'mm/dd/yyyy',
-      hiddenName: false
-   } );
+    $("#btn").click(function() {
+        $(".color").slideToggle("slow");
+    });
+});
 
-   $('#time').pickatime( {
-      formatSubmit: 'HH:i',
-      hiddenName: false
-   } );
-   /*
-   date/time picker
-   */
-         
-   /*
-   popup window
-   */
-   function deselect(e) {
-      $('.pop').slideFadeToggle(function() {
+
+$(document).ready(function() {
+    /*
+    date/time picker
+    */
+    $('#date').pickadate({
+        formatSubmit: 'mm/dd/yyyy',
+        hiddenName: false
+    });
+
+    $('#time').pickatime({
+        formatSubmit: 'HH:i',
+        hiddenName: false
+    });
+    /*
+    date/time picker
+    */
+
+    /*
+    popup window
+    */
+    function deselect(e) {
+        $('.pop').slideFadeToggle(function() {
             e.removeClass('selected');
-      });    
-   }
+        });
+    }
 
-   $(function() {
-      $('#create-new-reminder').on('click', function() {
-            if($(this).hasClass('selected')) {
-               deselect($(this));               
+    $(function() {
+        $('#create-new-reminder').on('click', function() {
+            if ($(this).hasClass('selected')) {
+                deselect($(this));
             } else {
-               $(this).addClass('selected');
-               $('.pop').slideFadeToggle();
+                $(this).addClass('selected');
+                $('.pop').slideFadeToggle();
             }
             return false;
-      });
+        });
 
-      $('.close').on('click', function() {
+        $('.close').on('click', function() {
             deselect($('#create-new-reminder'));
             return false;
-      });
-   });
+        });
+    });
 
-   $.fn.slideFadeToggle = function(easing, callback) {
-      return this.animate({ opacity: 'toggle', height: 'toggle' }, 'fast', easing, callback);
-   };
-   /*
-   end popup window
-   */
+    $.fn.slideFadeToggle = function(easing, callback) {
+        return this.animate({ opacity: 'toggle', height: 'toggle' }, 'fast', easing, callback);
+    };
+    /*
+    end popup window
+    */
 
-   //add-more button functionality
-   $('#add-more-subtasks').click(function(){
-      //maximum 5 subtasks
-      //make this variable static
-      if (typeof subtaskCount == 'undefined'){
+    //add-more button functionality
+    $('#add-more-subtasks').click(function() {
+        //maximum 5 subtasks
+        //make this variable static
+        if (typeof subtaskCount == 'undefined') {
             subtaskCount = 1;
-      }
+        }
 
-      //get all subtasks then store in array
-      let subtaskArray = []
-      $('.subtask-input').each(function(){
+        //get all subtasks then store in array
+        let subtaskArray = []
+        $('.subtask-input').each(function() {
             subtaskArray.push($(this).val());
-      });
+        });
 
-      //check if theres any empty one, and subtasks of more than 4
-      //if not, add subtask input field
-      if(subtaskArray.includes("")){
-         $('.subtask-input:last').attr('placeholder','Enter a subtask before adding more..');
-      }else if(subtaskCount >= 5){
-         
-      }
-      else{
-         input = $('<input type="text" placeholder="Enter subtask.." name="subtask-input" class="subtask-input">');
-         $('#subtasks-div').append(input);
-         subtaskCount++;
-      }
-   })
+        //check if theres any empty one, and subtasks of more than 4
+        //if not, add subtask input field
+        if (subtaskArray.includes("")) {
+            $('.subtask-input:last').attr('placeholder', 'Enter a subtask before adding more..');
+        } else if (subtaskCount >= 5) {
 
-   //create button functionality
-   $('#create').click(function(){
-      let title = $('#title').val();
-      console.log(title);
+        } else {
+            input = $('<input type="text" placeholder="Enter subtask.." name="subtask-input" class="subtask-input">');
+            $('#subtasks-div').append(input);
+            subtaskCount++;
+        }
+    })
 
-      //get date as a string
-      let date = $('#date').val();
-      console.log(date);
-      
-      //get time as a string
-      let time = $('#time').val();
-      console.log(time);
+    //create button functionality
+    $('#create').click(function() {
+        let title = $('#title').val();
+        console.log(title);
 
-      //store them together to a Date object
-      let dateObject = new Date(date +' '+time);
-      console.log(dateObject);
+        //get date as a string
+        let date = $('#date').val();
+        console.log(date);
 
-      //array to store subtasks
-      let subtaskArray = [];
-      //store all non-empty subtasks
-      $('.subtask').each(function(){
-            if($(this).val() != ''){
-               subtaskArray.push($(this).val());
+        //get time as a string
+        let time = $('#time').val();
+        console.log(time);
+
+        //store them together to a Date object
+        let dateObject = new Date(date + ' ' + time);
+        console.log(dateObject);
+
+        //array to store subtasks
+        let subtaskArray = [];
+        //store all non-empty subtasks
+        $('.subtask').each(function() {
+            if ($(this).val() != '') {
+                subtaskArray.push($(this).val());
             }
-      });
-      console.log(subtaskArray);
-   });
+        });
+        console.log(subtaskArray);
+    });
 });
